e4de126220655aa87e693018d27b80f85330f3c5
Carson <375162094@qq.com>
2022-07-01 02:20:33 +0800

    init

--- example/slider/index.tsx
+++ example/slider/index.tsx
@@ -12,14 +12,15 @@ import { SliderStyles } from './styles'
 
 const Slider = (props: any) => {
   const _markerRef = useRef<any>(null)
-  let _optionsArray = useRef(props.optionsArray || createArray(props.min, props.max, props.step)).current
-  let _initialValues = useRef(valueToPosition(props.values, _optionsArray, props.sliderLength)).current
+  const _optionsArrayRef = useRef(props.optionsArray || createArray(props.min, props.max, props.step))
+  const _initialValuesRef = useRef(valueToPosition(props.values, _optionsArrayRef.current, props.sliderLength))
   const _verticalRef = useRef(props.vertical)
+  const _enabledRef = useRef(props.enabled)
 
   const [trackBuffer, setTrackBuffer] = useState(0)
   const [curValue, setCurValue] = useState(props.values)
-  const [past, setPast] = useState(_initialValues)
-  const [position, setPosition] = useState(_initialValues)
+  const [past, setPast] = useState(_initialValuesRef.current)
+  const [position, setPosition] = useState(_initialValuesRef.current)
   const [pressed, setPressed] = useState(false)
   const _curValueRef = useRef(curValue)
   const _pastRef = useRef(past)
@@ -29,7 +30,7 @@ const Slider = (props: any) => {
   const Marker = props.customMarker
   const Label = props.customLabel
 
-  const _panResponder = useRef<any>(PanResponder.create({
+  const _panResponderRef = useRef<any>(PanResponder.create({
     onStartShouldSetPanResponder: (evt, gestureState) => true,
     onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
     onMoveShouldSetPanResponder: (evt, gestureState) => true,
@@ -40,45 +41,47 @@ const Slider = (props: any) => {
     onPanResponderRelease: (evt, gestureState) => onPanEnd(gestureState),
     onPanResponderTerminate: (evt, gestureState) => onPanEnd(gestureState),
     onShouldBlockNativeResponder: (evt, gestureState) => true,
-  })).current
-
-  console.log('1 props.vertical:', props.vertical);
+  }))
 
   useEffect(() => {
+    // 垂直滑动
     _verticalRef.current = props.vertical
-    console.log('change vertical:', _verticalRef.current);
   }, [props.vertical])
 
   useEffect(() => {
+    // 是否启用
+    _enabledRef.current = props.enabled
+  }, [props.enabled])
+
+  useEffect(() => {
     // 缓存进度
-    const _trackBuffer = valueToPosition(props.trackBuffer, _optionsArray, props.sliderLength)
+    const _trackBuffer = valueToPosition(props.trackBuffer, _optionsArrayRef.current, props.sliderLength)
     setTrackBuffer(_trackBuffer)
   }, [props.trackBuffer, props.sliderLength])
 
   useEffect(() => {
-    console.log('change pressed:', pressed);
-    if (pressed) { return }
-    _optionsArray = props.optionsArray || createArray(props.min, props.max, props.step)
-    const _position = valueToPosition(props.values, _optionsArray, props.sliderLength)
-    console.log('change props.values:', props.values);
+    if (_pressedRef.current) { return }
+
+    _optionsArrayRef.current = props.optionsArray || createArray(props.min, props.max, props.step)
+
+    const _position = valueToPosition(props.values, _optionsArrayRef.current, props.sliderLength)
+
     _curValueRef.current = props.values
     _pastRef.current = _position
     _positionRef.current = _position
+
     setCurValue(props.values)
     setPast(_position)
     setPosition(_position)
   }, [
-    props.min,
-    props.max,
-    props.step,
-    props.values,
-    props.sliderLength,
+    props.min, props.max,
+    props.step, props.values,
+    props.sliderLength, props.optionsArray
   ])
 
   const onPanGrant = () => {
     let __pressed = !_pressedRef.current
-    console.log('onPanGrant __pressed:', __pressed)
-    if (props.enabled) {
+    if (_enabledRef.current) {
       props.onValuesChangeStart && props.onValuesChangeStart()
       _pressedRef.current = __pressed
       setPressed(__pressed)
@@ -86,10 +89,10 @@ const Slider = (props: any) => {
   }
 
   const onPanMove = (gestureState: any) => {
-    console.log('onPanMove gestureState:', gestureState, _verticalRef.current)
-    if (!props.enabled) {
+    if (!_enabledRef.current) {
       return
     }
+
     const accumDistance = _verticalRef.current ? gestureState.dy : gestureState.dx
     const accumDistanceDisplacement = _verticalRef.current ? -gestureState.dx : gestureState.dy
     const unconfined = I18nManager.isRTL ? _pastRef.current - accumDistance : accumDistance + _pastRef.current
@@ -98,18 +101,12 @@ const Slider = (props: any) => {
     let confined = unconfined < bottom ? bottom : unconfined > top ? top : unconfined
     let slipDisplacement = props.touchDimensions.slipDisplacement
 
-    console.log('_pastRef.current:', _pastRef.current)
-    console.log('accumDistance:', accumDistance)
-    console.log('accumDistanceDisplacement:', accumDistanceDisplacement)
-    console.log('unconfined:', unconfined)
-    console.log('confined:', confined)
-    console.log('slipDisplacement:', slipDisplacement)
     if (
       Math.abs(accumDistanceDisplacement) < slipDisplacement ||
       !slipDisplacement
     ) {
-      _curValueRef.current = positionToValue(confined, _optionsArray, top)
-      let snapped = valueToPosition(_curValueRef.current, _optionsArray, top)
+      _curValueRef.current = positionToValue(confined, _optionsArrayRef.current, top)
+      let snapped = valueToPosition(_curValueRef.current, _optionsArrayRef.current, top)
       _positionRef.current = props.snapped ? snapped : confined
 
       setPosition(_positionRef.current)
@@ -124,9 +121,12 @@ const Slider = (props: any) => {
       props.onToggle()
       return
     }
+
     _pressedRef.current = !_pressedRef.current
+
     setPast(_positionRef.current)
     setPressed(_pressedRef.current)
+
     props.onValuesChangeFinish && props.onValuesChangeFinish(_curValueRef.current)
   }
 
@@ -201,7 +201,7 @@ const Slider = (props: any) => {
               <View
                 ref={_markerRef}
                 style={[SliderStyles.touch, touchStyle]}
-                {..._panResponder.panHandlers}
+                {..._panResponderRef.current.panHandlers}
               >
                 <Marker
                   pressed={pressed}
